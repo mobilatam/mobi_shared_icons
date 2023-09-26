a2f8fb1326db761aedf15c939f213901688b3c9f
MiguelGit-at-551210492275 <MiguelGit-at-551210492275>
2023-09-26 09:26:23 -0300

    [ADD] x icon

--- lib/src/svg/icons_data.dart
+++ lib/src/svg/icons_data.dart
@@ -16,6 +16,7 @@ class MobiSvgIcons {
   static const mobiLogoWhite = MobiIconData('assets/svg/mobi_logo_white.svg');
   static const mobiLogoColored = MobiIconData('assets/svg/mobi_logo_colored.svg');
   static const mobiMotoPurple = MobiIconData('assets/svg/moto_purple_icon.svg');
+  static const mobiXIcon = MobiIconData('assets/svg/x_icon.svg');
   static const calculatorsPurple = MobiIconData('assets/svg/calculators_purple.svg');
   static const addUserPurple = MobiIconData('assets/svg/add_user_purple.svg');
   static const surroundBox = MobiIconData('assets/svg/surround_box_purple.svg');
