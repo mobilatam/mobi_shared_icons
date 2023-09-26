f6e1cff4a40be4a807ec6b7481f88f8cc3234e28
Diego Emerson Salvatierra Berrios <sbd0029992@est.univalle.edu>
2023-09-26 10:39:11 -0400

    [FEATURE] add icons for widgets

--- lib/src/svg/icons_data.dart
+++ lib/src/svg/icons_data.dart
@@ -14,11 +14,17 @@ class MobiSvgIcons {
   static const compassOutline = MobiIconData('assets/svg/compass-outline.svg');
   static const compass = MobiIconData('assets/svg/compass.svg');
   static const mobiLogoWhite = MobiIconData('assets/svg/mobi_logo_white.svg');
-  static const mobiLogoColored = MobiIconData('assets/svg/mobi_logo_colored.svg');
+  static const mobiLogoColored =
+      MobiIconData('assets/svg/mobi_logo_colored.svg');
   static const mobiMotoPurple = MobiIconData('assets/svg/moto_purple_icon.svg');
-  static const calculatorsPurple = MobiIconData('assets/svg/calculators_purple.svg');
+  static const calculatorsPurple =
+      MobiIconData('assets/svg/calculators_purple.svg');
   static const addUserPurple = MobiIconData('assets/svg/add_user_purple.svg');
   static const surroundBox = MobiIconData('assets/svg/surround_box_purple.svg');
   static const purpleRider = MobiIconData('assets/svg/purple_rider.svg');
-  static const coloredCheckCircle = MobiIconData('assets/svg/colored_check_circle.svg');
+  static const coloredCheckCircle =
+      MobiIconData('assets/svg/colored_check_circle.svg');
+  static const pencil = MobiIconData('assets/svg/pencil.svg');
+  static const date = MobiIconData('assets/svg/date.svg');
+  static const moto = MobiIconData('assets/svg/moto.svg');
 }
