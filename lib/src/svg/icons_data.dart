cdd48bc921349939fa3ad25ed34933bfb7d73090
MiguelGit-at-551210492275 <MiguelGit-at-551210492275>
2023-09-27 19:29:35 -0300

    [ADD] arrow down icon

--- lib/src/svg/icons_data.dart
+++ lib/src/svg/icons_data.dart
@@ -23,5 +23,6 @@ class MobiSvgIcons {
   static const purpleRider = MobiIconData('assets/svg/purple_rider.svg');
   static const coloredCheckCircle = MobiIconData('assets/svg/colored_check_circle.svg');
   static const mobiArrowLeftIcon = MobiIconData('assets/svg/arrow_left_icon.svg');
+  static const mobiArrowDownIcon = MobiIconData('assets/svg/arrow_down_icon.svg');
   static const mobiHamburgerMenuIcon = MobiIconData('assets/svg/hamburger_menu.svg');
 }
